f5020a8f9f7ea0a9b3b901a2f3563c789627900b
Ethan Lee <elee@xpanxion.com>
2023-06-13 21:38:48 -0400

    Add carousel component to welcomepage

--- client/src/Pages/Welcome.js
+++ client/src/Pages/Welcome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Welcome.css";
+import Carousel from "../Carousel";
 
 export const Welcome = () => {
   return (
     <div className="title-container">
       <h1 className="welcome">Welcome!</h1>
       <h2>Save today, secure tomorrow!</h2>
+      <Carousel />
     </div>
   );
 };
