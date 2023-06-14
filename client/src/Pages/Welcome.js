bc4a51570fde410de536cde5da2ccf7630a5cfeb
Ethan Lee <elee@xpanxion.com>
2023-06-14 11:08:48 -0400

    pushing to dev

--- client/src/Pages/Welcome.js
+++ client/src/Pages/Welcome.js
@@ -6,7 +6,7 @@ export const Welcome = () => {
   return (
     <div className="title-container">
       <h1 className="welcome">Welcome!</h1>
-      <h2>Save today, secure tomorrow!</h2>
+      <h2>Save Today, Secure Tomorrow!</h2>
       <Carousel />
     </div>
   );
