612e9b62abc1781ac58d1d265063679491d89d17
Ethan Lee <elee@xpanxion.com>
2023-06-15 15:27:15 -0400

    Filter transactions

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -7,45 +7,94 @@ import "./Dashboard.css";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 5,
-    },
-  ],
-};
-
 export const Dashboard = () => {
+  const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/test");
+        const data = await res.json();
+        setTransaction(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTransactions();
+  }, []);
+
+  const expenses = transaction.filter((trans) => trans.type === "Expense");
+
+  const incomes = transaction.filter((trans) => trans.type === "Income");
+
+  console.log("Expenses: ", expenses);
+  console.log("Incomes: ", incomes);
+
+  const expenseData = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "category",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 5,
+      },
+    ],
+  };
+
+  const incomeData = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "category",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 5,
+      },
+    ],
+  };
+
   return (
     <div>
       <Header />
       <div>
         <div className="expenses">
           <h1>Expenses</h1>
-          <Doughnut data={data} />;
+          <Doughnut data={expenseData} />
         </div>
         <div className="incomes">
           <h1>Incomes</h1>
-          <Doughnut data={data} />;
+          <Doughnut data={incomeData} />
         </div>
       </div>
       <Footer />
